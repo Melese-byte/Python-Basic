02d1ae43b55a0a11596a1b95d4810e7b78f6f574
Melese <melesealemante02@gmail.com>
2025-07-20 03:39:22 -0700

    added loops

---
+++ loops.py
@@ -0,0 +1,12 @@
+#print a number 0-100
+#for loop 
+#For Loop: Used for repeating tasks over a range.
+for i in range(100):
+    print(i)
+    #the output is list the nubmer  1-99 Excluding 100
+#While Loop: Runs as long as a condition is True.
+countdown=5;
+while countdown>0:#if countdown is 0 terminate from the loop.
+    print("count down",countdown)
+    countdown-=1 # countdown=countdown-1;
+print("Blast Off") 
