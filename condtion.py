61b25dc27ee6a65c480ad41692c37aae2e791068
Melese <melesealemante02@gmail.com>
2025-07-20 03:17:45 -0700

    initial commit

---
+++ condtion.py
@@ -0,0 +1,17 @@
+temprature=int(input("Enter the tempratue"));
+if temprature>45:
+    print("the day is hot day");
+else:
+    print("the day is cool day")
+    #Grading 
+marks=float(input("Enter students mark"));
+if(marks>90):
+        print("Grade: Great distniction");
+elif(marks<90 and marks>80):
+        print("Grade: Distnictions");
+elif(marks<80 and marks >75):
+        print("Grade: Credit");
+else:
+        print("Grade: fail");
+        
+         
