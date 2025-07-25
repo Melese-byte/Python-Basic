ad6ec82834687417f8a4145f2ef8cd616e8740ca
Melese <melesealemante02@gmail.com>
2025-07-25 02:28:09 -0700

    modules

---
+++ modules.py
@@ -0,0 +1,7 @@
+#Modules help you get things done faster—like using ready-made gadgets!
+#math modules for mathematical functions
+import math
+import random
+print(math.sqrt(16));
+dice_roll=random.randint(1,6);
+print("random dice", dice_roll);
