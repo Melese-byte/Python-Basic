770ac2948720135c71ad8a92393b6bf0892545ec
Melese <melesealemante02@gmail.com>
2025-07-25 03:10:44 -0700

    Greatgreating app

---
+++ greating_app/app.py
@@ -0,0 +1,9 @@
+
+"""
+Create a program that asks for the user’s name and favorite color, 
+then prints a personalized greeting like:
+“Hello, [Name]! Your favorite color, [Color], is awesome!”
+"""
+username=input("Enter the user name ");
+favorite_color=input("Enter your favorite color ");
+print("Hello", username+"!" + "Your favorite color", favorite_color + "is awesome!");
