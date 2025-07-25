7410fd33cf007e08191ce95e892b208aaab312ed
Melese <melesealemante02@gmail.com>
2025-07-25 02:27:26 -0700

    add even or odd

--- functions.py
+++ functions.py
@@ -12,4 +12,10 @@ def mult(x,y):
 result=mult(8,9);
 print("The multiplications of  8 and 9 is",result)
 
-
+#Q. Write a Python function that checks if a number is even or odd and prints the result. Here's a start:
+def even_odd(x):
+    if x%2==0:
+        print("The number is even");
+    else:
+        print("The number is odd");
+even_odd(4);
