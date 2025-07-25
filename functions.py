9f7487b93b4f649b8dbd72fe7d592cdd870bb027
Melese <melesealemante02@gmail.com>
2025-07-25 02:15:24 -0700

    added multiplication and add functions

--- functions.py
+++ functions.py
@@ -2,4 +2,14 @@ def great(name):
     print("hello," + name);
 great("melese");
 great("Birhanu");
-#the 
+#the followingis how to return the vlaue
+
+def add(x,y):
+    return(x+y);
+print(add(8,9));
+def mult(x,y):
+    return(x*y);
+result=mult(8,9);
+print("The multiplications of  8 and 9 is",result)
+
+
